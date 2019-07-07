dc4d8b62aa7741196cdccf1c70ea00bbd3d096de
Kay McCormick <kay@kaymccormick.com>
2019-07-07 19:17:17 +0000

    update

--- model/reducers/classModel.ts
+++ model/reducers/classModel.ts
@@ -1,8 +1,18 @@
 import { ClassModelState, Action} from '../types';
 import { classModel as initialClassModelState } from '../ApplicationState';
-export default (args: any) => (state: ClassModelState = initialClassModelState, action: Action): ClassModelState | undefined => {
-switch(action.type) {
-default:
-return state;
-}
+import {LOAD_PROJECTS} from "../actions/classModel";
+
+import RestClient from '@heptet/rest-client'
+
+interface Args { restClient: RestClient }
+export default (args: Args) => (state: ClassModelState = initialClassModelState, action: Action): ClassModelState | undefined => {
+    switch(action.type) {
+        case LOAD_PROJECTS:
+        console.log('load projectss');
+            args.restClient.getProjects().then(projects => {
+                return Object.assign({},state, {projects});
+            })
+        default:
+            return state;
+    }
 };
