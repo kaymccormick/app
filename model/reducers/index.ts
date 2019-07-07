5fce97db36e83c0fd499c4a234797977489b1906
Kay McCormick <kay@kaymccormick.com>
2019-07-07 19:18:15 +0000

    update

--- model/reducers/index.ts
+++ model/reducers/index.ts
@@ -1,12 +1,13 @@
 import { combineReducers } from 'redux';
-import{ModelFactory,ClassModelFactory} from '../types';
+import{ModelFactory} from '../types';
 import entityViewUI from './entityViewUI';
 import classModel from './classModel';
 import model from './model';
+import RestClient from '@heptet/rest-client'
 
-type Args= {
-modelFactory: ModelFactory;
-classModel: {};
+interface Args {
+    modelFactory: ModelFactory;
+    classModel: { restClient: RestClient };
 }
 
 export default (args: Args): any => combineReducers( { entityViewUI, model: model(args.modelFactory), classModel: classModel(args.classModel) } )
