a24c5763c88a1210759465d300f33f293d3acc57
Kay McCormick <kay@kaymccormick.com>
2019-07-07 01:43:43 +0000

    update

---
+++ entity/logging/Log.ts
@@ -0,0 +1,6 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import UniversalBase from '../core/UniversalBase';
+
+@Entity()
+export class Log extends UniversalBase {
+}
---
+++ entity/logging/index.ts
@@ -0,0 +1,3 @@
+export { Log } from './Log';
+export { LogEntry } from './LogEntry';
+export { Component } from './Component';
