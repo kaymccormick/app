7abc0fcc3c536edd9145747d41b096d52c2a52bc
Kay McCormick <kay@kaymccormick.com>
2019-07-19 01:39:23 +0000

    update

--- entry/entityView.tsx
+++ entry/entityView.tsx
@@ -1,19 +1,26 @@
+/**
+* Entry point to the main application. A lot of code smells right now
+* due to partially implemented functionality, or remants thereof.
+ *
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, combineReducers,compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import Site from '../site/Site';
-import RestClient from '@heptet/rest-client'
-import { Configuration } from '../src/Configuration';
-import App from "../components/App";
-import { WebApplication } from '../src/WebApplication';
 import HTML5Backend from 'react-dnd-html5-backend';
 import { DndProvider } from 'react-dnd';
 
+import RestClient from '@heptet/rest-client'
+
+import Site from '../site/Site';
+import { Configuration } from '../src/Configuration';
+import App from "../components/App";
+import { WebApplication } from '../src/WebApplication';
+
+// hardcoded url for now
 const restClient = new RestClient({baseUri: '/cme', logDebug: (arg) => {}});
 const configJs = require('../core.conf').default;
-console.log(configJs);
 const app = new WebApplication({ config: configJs, restClient });
 app.init();
 
