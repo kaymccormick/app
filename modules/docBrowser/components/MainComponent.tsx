c050fe4fd42faa065eb03c0ed51b3ce89f8a533b
Kay McCormick <kay@kaymccormick.com>
2019-07-19 02:58:50 +0000

    new docBrowser module

---
+++ modules/docBrowser/components/MainComponent.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import ModuleContainer from './containers/ModuleContainer';
+// @ts-ignore
+export default (props) => <ModuleContainer {...props}/>;
---
+++ modules/docBrowser/components/containers/ModuleContainer.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import { ApplicationState } from '../../../../model/types';
+import { Module } from '../../Module';
+
+export interface MyProps {
+    module?: any;
+}
+
+const mapStateToProps = (state: ApplicationState): MyProps => ({
+});
+
+// @ts-ignore
+const mapDispatchToProps = (dispatch: any, ownProps: MyProps) => {
+    if(!ownProps.module) {
+        throw new Error('');
+    }
+    const m = ownProps.module as Module;
+    if(!m) {
+        throw new Error('no module');
+    }
+    const actions = m.actions;
+    return {
+    };
+};
+
+//@ts-ignore
+export default connect(mapStateToProps, mapDispatchToProps)(() => <div/>);
